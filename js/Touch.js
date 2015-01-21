3016e66e79f2b74d3aad3d76bbdf2ed707e7b716
Soul <soulued@my.com>
2015-01-21 21:20:59 +0800

    fix getting touchmove state bug

--- js/Touch.js
+++ js/Touch.js
@@ -2,10 +2,9 @@
  * Created by margintan on 15/1/11.
  */
 (function (win, doc){
-    var _touchObject, _forEach;
+    var _touchObject;
 
     _touchObject =  {};
-    _forEach = Array.prototype.forEach;
 
 
     function combinationNodeNameIdClassNameByElement (_element) {
@@ -30,7 +29,6 @@
         var _stringCallback;
 
         if (!_callback) {
-
             if (!_touchObject[_elementRepresent].elementHasEvent[_elementRepresent]) {
                 _touchObject[_elementRepresent].elementHasEvent[_elementRepresent] = {}
             }
@@ -94,40 +92,20 @@
     }
 
     function getFireEvent (e, _elementRepresent) {
-        var _element, _touchEventObject, isHasEvent, _id, _className, _nodeName, _prototypeString, _REX;
+        var isHasEvent;
 
         isHasEvent = {
-            "tap": []
+            "tap": [],
+            "slideUp": []
         };
 
-        _element = e.target;
-
-        _id = _element.id && "#" + _element.id;
-        _nodeName = _element.nodeName.toLocaleLowerCase();
-        _className = _element.className && ("." + _element.className.replace(/\s/g, "."));
-
-        _touchEventObject = _touchObject[_elementRepresent].elementHasEvent;
-
-        /*_REX = "/\w*#{0,1}\w*[.\w*]*";*/
-
-        _REX =  new RegExp(getFireEvent._createRex(_nodeName, _id, _className));
-
-        for (var i in _touchEventObject) {
-            if ( i  == i.match(_REX)[0]) {
-                for (var k in _touchEventObject[i]) {
-                    for (var z in _touchEventObject[i][k]) {
-                        isHasEvent[k].push(_touchEventObject[i][k][z]);
-                    }
-
-                }
-            }
-        }
+        getFireEvent._popUp(e.target, _elementRepresent, isHasEvent);
 
         return isHasEvent ? isHasEvent : false;
     }
 
     getFireEvent._createRex = function (_nodeName, _id, _className) {
-        var _classNameArr = _className.split("."),
+        var _classNameArr = _className && _className.split("."),
             _rex;
 
         _rex = "[" + _nodeName + "]*";
@@ -136,54 +114,104 @@
             _rex = _rex + "[" + _id + "]{0,1}"
         }
 
-        _rex = _rex + "[";
-        _classNameArr.forEach(function(value){
-            if (value) {
-                value = "." + value + "|";
-                _rex = _rex + value;
-            }
-        });
+        if (_classNameArr) {
+            _rex = _rex + "[";
 
-        _rex  = _rex + "]*";
+            _classNameArr.forEach(function(value){
+                if (value) {
+                    value = "." + value + "|";
+                    _rex = _rex + value;
+                }
+            });
+            _rex  = _rex + "]*";
+        }
 
-        return _rex;
+        return _rex.replace(/\-/g, "\\-");
     };
 
+    getFireEvent._popUp = function (target, _elementRepresent, isHasEvent) {
+        var _element, _touchEventObject, _id, _className, _nodeName, _REX;
+
+        _element = target;
+
+        _id = _element.id && "#" + _element.id;
+        _nodeName = _element.nodeName.toLocaleLowerCase();
+        _className = _element.className && ("." + _element.className.replace(/\s/g, "."));
+
+        _touchEventObject = _touchObject[_elementRepresent].elementHasEvent;
+
+        _REX =  new RegExp(getFireEvent._createRex(_nodeName, _id, _className));
+
+        for (var i in _touchEventObject) {
+            if ( i  == i.match(_REX)[0]) {
+                for (var k in _touchEventObject[i]) {
+                    for (var z in _touchEventObject[i][k]) {
+                        isHasEvent.target = target;
+                        isHasEvent[k].push(_touchEventObject[i][k][z]);
+                    }
+                }
+            }
+        }
+
+        if (target.parentNode) {
+            getFireEvent._popUp(target.parentNode, _elementRepresent, isHasEvent);
+        }else{
+            return false;
+        }
+
+    };
 
     function Touch(_elementRepresents, _element, _event, _proxy, _callback) {
-        var _elementRepresent = _elementRepresents, thisActionType;
+        var _elementRepresent, thisActionType, startX, startY, endX, endY, _slideUpFireDistance;
+
+        _elementRepresent = _elementRepresents;
 
         _touchObject[_elementRepresent] = {};
         _touchObject[_elementRepresent].elementHasEvent = {};
         _touchObject[_elementRepresent]._touchObject = this;
 
+        _slideUpFireDistance = 20;
 
         function handleStart (e) {
+            e.preventDefault();
+            var _eToucheObject;
+
+            _eToucheObject = e.touches[0];
             thisActionType = {};
             thisActionType.start = e.type;
+
+            startX = _eToucheObject.clientX;
+            startY = _eToucheObject.clientY;
         }
 
-        var handleMove = debounce(function (e) {
+        function handleMove (e) {
+            e.preventDefault();
             thisActionType.move = e.type;
-        }, 100);
+        }
 
         function handleEnd (e) {
-            var isHasEvent;
+            e.preventDefault();
+            var isHasEvent, _eTouchObject, _slideUpDistance;
             thisActionType.end = e.type;
+
+            _eTouchObject = e.changedTouches[0];
 
             isHasEvent = getFireEvent(e, _elementRepresent);
 
+            endX = _eTouchObject.clientX;
+            endY = _eTouchObject.clientY;
+
+            _slideUpDistance = startY - endY;
+
             if (!isHasEvent) return;
 
-            if (!thisActionType.move && isHasEvent.tap) {
-                isHasEvent.tap.forEach(function (value){
-                    value.call(e.target, e);
-                });
+            if (!thisActionType.move && isHasEvent.tap[0]) {
+                Touch._fireEvent.call(isHasEvent.tap, e, isHasEvent.target);
                 return false;
             }
 
-            if (thisActionType.move) {
-
+            if (thisActionType.move && _slideUpDistance > _slideUpFireDistance && isHasEvent.slideUp[0]) {
+                Touch._fireEvent.call(isHasEvent.slideUp, e, isHasEvent.target);
             }
         }
 
@@ -192,13 +220,16 @@
         _element.addEventListener("touchend", handleEnd, false);
 
         addCallbackTo(_elementRepresent, _event, _proxy, _callback);
-
     }
 
+    Touch._fireEvent = function (e, target) {
+        this.forEach(function (value) {
+            value.call(target, e);
+        })
+    };
 
     var _Touch = function (_element, _event, _proxy, _callback) {
         var _elementRepresent = combinationNodeNameIdClassNameByElement(_element);
-
         if (_touchObject[_elementRepresent]) {
             addCallbackTo(_elementRepresent, _event, _proxy, _callback);
             return _touchObject[_elementRepresent]._touchObject;
@@ -207,19 +238,5 @@
         }
     };
 
-    _Touch(document.body, "tap", function (){
-        alert("123");
-    });
-    _Touch(document.body, "tap", ".test", function (){
-        alert("123");
-    });
-    _Touch(document.body, "tap", "p#test.test.test1.test3.test4", function (){
-        alert("123");
-    });
-    _Touch(document.body, "tap", "p#test", function (){
-        alert("123");
-    });
-
-
-
+    SX.Touch = _Touch;
 })(window, document);
